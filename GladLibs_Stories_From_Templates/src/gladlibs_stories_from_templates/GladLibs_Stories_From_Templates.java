722d8633bf56fe34c9f2e8ff2e2a83c21a09844c
Evegenn <demerbrise@gmail.com>
2015-12-07 16:43:40 +0300

    Programming Exercise: Telling a Random Story Assignment 1: Most Frequent Word has been completed.

--- GladLibs_Stories_From_Templates/src/gladlibs_stories_from_templates/GladLibs_Stories_From_Templates.java
+++ GladLibs_Stories_From_Templates/src/gladlibs_stories_from_templates/GladLibs_Stories_From_Templates.java
@@ -10,12 +10,29 @@ package gladlibs_stories_from_templates;
  * @author Lartsev
  */
 public class GladLibs_Stories_From_Templates {
-
+    
+    
+    public void testerFindUnique() {
+        WordFrequencies wF = new WordFrequencies();
+        wF.findUnique();
+        int sizeOf = wF.getMyWords().size();
+        System.out.println(sizeOf);
+        for(int i=0; i<sizeOf; i++){
+             System.out.print(wF.getMyWords().get(i) + "\t");
+             System.out.println(wF.getMyFreqs().get(i));
+        }
+        System.out.println();
+        System.out.println(wF.findIndexOfMax());
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        GladLibs_Stories_From_Templates ex = new GladLibs_Stories_From_Templates ();
+        ex.testerFindUnique();
+        
     }
     
 }
---
+++ GladLibs_Stories_From_Templates/src/gladlibs_stories_from_templates/WordFrequencies.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gladlibs_stories_from_templates;
+
+import edu.duke.FileResource;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ *
+ * @author Lartsev
+ */
+public class WordFrequencies {
+    private ArrayList<String> myWords;
+    private ArrayList<Integer> myFreqs;
+    private final String folder = "data/";
+    private final String filename = "testwordfreqs.txt";
+    
+    public WordFrequencies() {
+        myWords = new ArrayList<String>();
+        myFreqs = new ArrayList<Integer>();
+    }
+    
+    public void findUnique() {
+        myWords.clear();
+        myFreqs.clear();
+        FileResource fr = new FileResource(folder+filename);
+        for(String word : fr.words()){
+            String wordLow = word.toLowerCase();
+            if(!myWords.contains(wordLow)) {
+                myWords.add(wordLow);
+                myFreqs.add(1);
+            } else {
+                int indexList = myWords.indexOf(wordLow);
+                int next = myFreqs.get(indexList);
+                myFreqs.set(indexList, next+1);
+            }
+        }
+    }
+    /**
+     * find max value in ArrayList<Integer> 
+     * @return max value
+     */
+    public int findIndexOfMax() {
+        Collections.sort(myFreqs);
+        return myFreqs.get(myFreqs.size() - 1);
+    
+    }
+    
+    public ArrayList<String> getMyWords() {
+        return myWords;
+    }
+    public ArrayList<Integer> getMyFreqs() {
+        return myFreqs;
+    }
+}
