0e95588e2d267dbfd0265eb30a0e71548ad2da5f
Evegenn <demerbrise@gmail.com>
2015-12-07 15:44:38 +0300

    Start new programming assignment - for week 2

---
+++ GladLibs_Stories_From_Templates/src/gladlibs_stories_from_templates/GladLibs_Stories_From_Templates.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gladlibs_stories_from_templates;
+
+/**
+ *
+ * @author Lartsev
+ */
+public class GladLibs_Stories_From_Templates {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
