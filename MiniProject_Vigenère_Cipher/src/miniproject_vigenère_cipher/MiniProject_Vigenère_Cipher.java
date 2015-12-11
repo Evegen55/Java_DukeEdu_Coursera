4c94dfa8c16df1985d056543b98e75824d14c98d
Evegenn <demerbrise@gmail.com>
2015-12-11 17:25:03 +0300

    add some projects

---
+++ MiniProject_Vigenère_Cipher/src/miniproject_vigenère_cipher/MiniProject_Vigenère_Cipher.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miniproject_vigenère_cipher;
+
+/**
+ *
+ * @author Lartsev
+ * 
+ * In this module, you will develop a program to break the Vigenère Cipher, 
+ * a more complex version of the Caesar Cipher. You will improve your program 
+ * in three stages: first decrypting messages where you know the language and key length,
+ * then adding the capability to handle messages with unknown key length, 
+ * then extending the program to handle messages in a range of possible languages. 
+ * Through this project, you will be able 
+ * (1) to effectively use HashSet and HashMap, 
+ * (2) to expand an algorithm from solving a simpler problem to handle broader, 
+ *     more complex problems, and 
+ * (3) to design and modify program code involving a more complex collection 
+ *     of classes, methods, and data.
+ */
+public class MiniProject_Vigenère_Cipher {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
