1fc780d59affb494312251fcceb0035654e5ebdd
Evegenn <demerbrise@gmail.com>
2015-12-17 18:01:15 +0300

    add some files in the data folder.

--- MiniProject_Vigenere_Cipher/src/miniproject_vigenere_cipher/VigenereBreaker.java
+++ MiniProject_Vigenere_Cipher/src/miniproject_vigenere_cipher/VigenereBreaker.java
@@ -27,6 +27,24 @@ public class VigenereBreaker {
         }
         return sb.toString();
     }
+    
+    /**
+     * Write the public method tryKeyLength, which takes three parameters—a String
+     * encrypted that represents the encrypted message, an integer klength that represents
+     * the key length, and a character mostCommon that indicates the most common
+     * character in the language of the message. This method should make use of the
+     * CaesarCracker class, as well as the sliceString method, to find the shift for each index
+     * in the key. You should fill in the key (which is an array of integers) and return it. 
+     * 
+     * Test this method on the text file athens_keyflute.txt , which is a scene from 
+     * "A Midsummer Night’s Dream" encrypted with the key “flute”, and make sure 
+     * you get the key {5, 11, 20, 19, 4}.
+     * 
+     * @param encrypted
+     * @param klength
+     * @param mostCommon
+     * @return 
+     */
 
     public int[] tryKeyLength(String encrypted, int klength, char mostCommon) {
         int[] key = new int[klength];
